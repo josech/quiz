7afa9064b23b3793c3312b91eb879ccbefdcb381
Jose Martinez <xjosechx@gmail.com>
2015-07-13 00:23:32 -0500

    Primera pregunta

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
@@ -17,9 +17,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
