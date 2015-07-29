256539b0ad6f019ad828ca9ef692ed5d320878f0
Jose Martinez <xjosechx@gmail.com>
2015-07-28 23:46:53 -0500

    Base de Datos

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,8 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
 router.get('/author/author', quizController.author);
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
